7906527d703ec6a61baecd0cfae8ad6a0d2b4f72
Francisco Ramos <fro.profesional@gmail.com>
2023-04-18 16:52:37 -0700

    Deploy

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "../../svc/mod.ts";
+export * from "https://raw.githubusercontent.com/clau-org/mod-core/v0.0.10/mod.ts";
 export { default as z } from "https://deno.land/x/zod@v3.21.4/index.ts";
